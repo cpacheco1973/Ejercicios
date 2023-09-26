4f80e5aceaf29ef083a7d0c0681254b34eb9a8e5
cpacheco1973 <pachemc@hotmail.com>
2023-09-25 20:56:35 -0600

    Ejercicio 1 listo

--- UPITarea1Prog2/UPITarea1Prog2/Program.cs
+++ UPITarea1Prog2/UPITarea1Prog2/Program.cs
@@ -13,7 +13,7 @@ namespace UPITarea1Prog2
             int opcion = 0;
             do
             {
-                Console.WriteLine("1 - Ejercicio 1");
+                Console.WriteLine("1 - Ejercicio 1 - Camisas");
                 Console.WriteLine("2 - Ejercicio 2");
                 Console.WriteLine("3 - Ejercicio 3");
                 Console.WriteLine("4 - Ejercicio 4");
@@ -22,14 +22,69 @@ namespace UPITarea1Prog2
 
                 switch (opcion)
                 {
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
+                    case 1: 
+                        Console.Clear();    
+                        Ejercicio1();
+                        break;
+                    case 2: 
+                        Console.Clear();    
+                        Ejercicio2();
+                        break;  
+                    case 3: 
+                        Console.Clear();    
+                        Ejercicio3();
+                        break;
                     default:
                         break;
                 }
 
             } while (opcion != 4);
+        }
+
+        public static void Ejercicio1()  //camisas.
+        {
+            float precio = 0f;
+            int cantidad = 0;   
+            float total  = 0f;  
+
+            Console.WriteLine("Digite el precio de la camisa: ");
+            precio = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            if (cantidad == 1) 
+            { 
+                Console.WriteLine($"Total a pagar por {cantidad} camisas es: {cantidad * precio}");
+            }  
+            
+            else if (cantidad > 1 && cantidad < 6)
+                {
+                    total = (cantidad * precio) * 0.15f;
+                    total = (cantidad * precio) - total;
+                    Console.WriteLine($"Total a pagar por {cantidad} camisas es: {total} con descuento de 15%");
+                } 
+            
+                else if (cantidad > 5)
+                    {
+                        total = (cantidad * precio) * 0.20f;
+                        total = (cantidad * precio) - total;
+                        Console.WriteLine($"Total a pagar por {cantidad} camisas es: {total} con descuento de 20%");
+                    }        
+        
+        }
+
+        public static void Ejercicio2()
+        {
+
+            Console.WriteLine("Ejercicio 2");
+
+        }
+
+        public static void Ejercicio3()
+        {
+
+            Console.WriteLine("Ejercicio 3");
+
         }
     }
 }
