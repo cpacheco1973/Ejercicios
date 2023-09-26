cefad284873f5f776d1ce029a62c0c656760e4e0
cpacheco1973 <pachemc@hotmail.com>
2023-09-25 20:21:59 -0600

    Tarea 2 - Version Inicial

---
+++ UPITarea1Prog2/UPITarea1Prog2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPITarea1Prog2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0
+            do
+            {
+                Console.WriteLine("1 - Ejercicio 1");
+                Console.WriteLine("2 - Ejercicio 2");
+                Console.WriteLine("3 - Ejercicio 3");
+                Console.WriteLine("4 - Ejercicio 4");
+                Console.WriteLine("Digite una opcion");
+                opcion = int.Parse(Console.ReadLine()); 
+
+
+            } while (opcion != 4);
+        }
+    }
+}
