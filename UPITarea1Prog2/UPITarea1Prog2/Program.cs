2df9b47c20b231317372b99dd642f3dc87b83d4f
cpacheco1973 <pachemc@hotmail.com>
2023-09-26 21:53:39 -0600

    Ejercicio 3 listo.

--- UPITarea1Prog2/UPITarea1Prog2/Program.cs
+++ UPITarea1Prog2/UPITarea1Prog2/Program.cs
@@ -13,11 +13,15 @@ namespace UPITarea1Prog2
             int opcion = 0;
             do
             {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("          TAREA 2");
+                Console.WriteLine("----------------------------");
                 Console.WriteLine("1 - Ejercicio 1 - Camisas");
-                Console.WriteLine("2 - Ejercicio 2");
-                Console.WriteLine("3 - Ejercicio 3");
-                Console.WriteLine("4 - Ejercicio 4");
-                Console.WriteLine("Digite una opcion");
+                Console.WriteLine("2 - Ejercicio 2 - Estudiante");
+                Console.WriteLine("3 - Ejercicio 3 - Productos");
+                Console.WriteLine("4 - Salir");
+                Console.WriteLine("----------------------------");
+                Console.Write("Digite una opcion: ");
                 opcion = int.Parse(Console.ReadLine());
 
                 switch (opcion)
@@ -25,17 +29,25 @@ namespace UPITarea1Prog2
                     case 1: 
                         Console.Clear();    
                         Ejercicio1();
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
                     case 2: 
                         Console.Clear();    
                         Ejercicio2();
-                        break;  
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
                     case 3: 
                         Console.Clear();    
                         Ejercicio3();
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
-                    default:
-                        break;
+                    
                 }
 
             } while (opcion != 4);
@@ -47,43 +59,60 @@ namespace UPITarea1Prog2
             int cantidad = 0;   
             float total  = 0f;  
 
-            Console.WriteLine("Digite el precio de la camisa: ");
+            Console.Write("Digite el precio de la camisa: ");
             precio = float.Parse(Console.ReadLine());
-            Console.WriteLine("Digite la cantidad: ");
+            Console.Write("Digite la cantidad: ");
             cantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
 
             if (cantidad == 1) 
             { 
-                Console.WriteLine($"Total a pagar por {cantidad} camisas es: {cantidad * precio}");
+                Console.WriteLine($"Total a pagar por {cantidad} camisas es ${cantidad * precio}");
             }  
             
             else if (cantidad > 1 && cantidad < 6)
                 {
                     total = (cantidad * precio) * 0.15f;
                     total = (cantidad * precio) - total;
-                    Console.WriteLine($"Total a pagar por {cantidad} camisas es: {total} con descuento de 15%");
+                    Console.WriteLine($"Total a pagar por {cantidad} camisas es ${total} con descuento de 15%");
                 } 
             
                 else if (cantidad > 5)
                     {
                         total = (cantidad * precio) * 0.20f;
                         total = (cantidad * precio) - total;
-                        Console.WriteLine($"Total a pagar por {cantidad} camisas es: {total} con descuento de 20%");
-                    }        
-        
+                        Console.WriteLine($"Total a pagar por {cantidad} camisas es ${total} con descuento de 20%");
+                    }
+
+            Console.WriteLine(" ");
         }
 
-        public static void Ejercicio2()
+        public static void Ejercicio2() // estudiante
         {
 
             Console.WriteLine("Ejercicio 2");
 
         }
 
-        public static void Ejercicio3()
+        public static void Ejercicio3() // productos
         {
+            int cantidad = 0;
+            
+            Console.Write("Digite la cantidad de productos: ");
+            cantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            
+            if ((cantidad > 1 && cantidad < 11))
+            {
+                Console.WriteLine($"Total a pagar por {cantidad} productos a $20 es: ${cantidad * 20}");
+            }
 
-            Console.WriteLine("Ejercicio 3");
+            else if (cantidad > 10)
+            {
+                Console.WriteLine($"Total a pagar por {cantidad} productos a $15 es: ${cantidad * 15}");
+            }
+            
+            Console.WriteLine(" ");
 
         }
     }
