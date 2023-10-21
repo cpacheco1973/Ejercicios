15f13ba2d903553f27d5d352bd0ac3b30ad200e4
cpacheco1973 <pachemc@hotmail.com>
2023-10-21 16:40:10 -0600

    Carga Inicial Practica Examen - 02 oct

---
+++ PracticaExamen/PracticaExamen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExamen
+{
+    internal class Program
+    {
+        static float num1, num2; //variables globales de la clase.
+        static void Main(string[] args)
+        {
+            // variables globales y locales.
+            int total;
+
+            suma();
+            resta();
+            Console.Read();
+
+        }
+
+        static void suma() 
+        {
+            float resultado; // variable local       
+            num1 = 3;
+            num2 = 9;
+
+            Console.WriteLine($"Suma: {num1 + num2}");
+        
+        }
+
+        static void resta()
+        {
+            num1 = 1; num2 = 2;
+            Console.WriteLine($"Resta: {num1 - num2}");
+
+        }
+
+    }
+}
